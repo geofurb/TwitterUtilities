cf6861e2a1bc273c2951b22029ab15dd1c6ec737
geofurb <geofurb@gmail.com>
2020-02-16 05:22:44 -0500

    Formatting and resume-task improvements

--- src/blockscanner.py
+++ src/blockscanner.py
@@ -12,11 +12,16 @@ SKIP_VERIFIED = True
 OUTPUT = 'blockscan_results.txt'
 
 last_id = None
-with open(f'data/{OUTPUT}') as fin:
-    for line in fin:
-        line = line.strip()
-        if line and line != 'ID':
-            last_id = int(line.split('\t')[0])
+try:
+    with open(f'data/{OUTPUT}') as fin:
+        for line in fin:
+            line = line.strip()
+            if line and line[0:2] != 'ID':
+                last_id = int(line.split('\t')[0])
+except FileNotFoundError:
+    print('Output file not found. Creating: data/'+OUTPUT)
+    with open(f'data/{OUTPUT}', 'w+') as fout:
+        pass
 
 # Load Twitter API Keys
 with open('secrets.json') as fin:
@@ -46,9 +51,9 @@ BLOCKS = 'blocks/ids.json'
 LOOKUP = 'users/lookup.json'
 FRIENDS = 'friends/ids.json'
 
-with getSession() as sess, open(f'data/{OUTPUT}', 'w+') as fout:
+with getSession() as sess, open(f'data/{OUTPUT}', 'a+') as fout:
     # Verify credentials
-    print('Verifying credentials...', end='\t', flush=True)
+    print('Verifying credentials...\t', end='\t', flush=True)
     try:
         resp = sess.get(ACCOUNT)
         data = resp.json()
@@ -59,14 +64,14 @@ with getSession() as sess, open(f'data/{OUTPUT}', 'w+') as fout:
     print('Done!')
 
     # Get blocked users
-    print('Tabulating blocked users...', end='\t', flush=True)
+    print('Tabulating blocked users...\t', end='\t', flush=True)
     resp = sess.get(BLOCKS)
     data = resp.json()
     blocked_users = set(data["ids"])
     print('Done!')
 
     # Get own followers
-    print('Tabulating followers...', end='\t', flush=True)
+    print('Tabulating followers...\t\t', end='\t', flush=True)
     followers = []
     cursor = -1             # Iterate pages in case more than 5000 followers
     while cursor != 0:
@@ -85,22 +90,23 @@ with getSession() as sess, open(f'data/{OUTPUT}', 'w+') as fout:
                 resp = sess.post(LOOKUP, data={'user_id': ','.join([str(x) for x in follower]), 'include_entities': False})
                 data = resp.json()
                 for user in data:
-                    users.append((user['id'], user['screen_name'], user['verified'], user['friends_count'], user['followers_count']))
+                    users.append((user['id'], user['screen_name'], user['verified'], user['friends_count'], user['followers_count'], user['protected']))
                 break
             except:
                 if attempt == 2:
                     print(f'\nThere was a problem getting the metadata for some followers. Skipping these:\n{follower}')
 
-    print('Done!')
+    print('Done!\n')
 
     # Get friends of each follower, count matches from your blocked users
     print('Tabulating blocked friends for each of your followers. This might take a long time.')
     users = sorted(users, key=lambda x:x[3])
     if last_id is not None:
+        users = users[([x[0] for x in users]).index(last_id)+1:]
+        print(f'Resuming with @{users[0][1]}...')
+    else:
         fout.write('ID\tUSERNAME\tBLOCKED FRIENDS\tFRIENDS\tFOLLOWERS\tVERIFIED\n')
-        users = users[([x[0] for x in users]).index(last_id):]
-        print(f'Resuming with {users[0][1]}.')
-    print('ID\tUSERNAME\tBLOCKED FRIENDS\tFRIENDS\tFOLLOWERS\tVERIFIED')
+    print('ID\t\t\t\t\t\tUSERNAME\t\tBLOCKED FRIENDS\t\tFRIENDS\t\tFOLLOWERS\tVERIFIED')
     for user in users:
         for attempt in range(3):
             try:
@@ -108,11 +114,23 @@ with getSession() as sess, open(f'data/{OUTPUT}', 'w+') as fout:
                 resp = sess.get(FRIENDS, params={'user_id': user[0]})
                 data = resp.json()
                 blocked_friends = len(blocked_users.intersection(data['ids']))  # Count the number of their friends that you've blocked
-                fout.write(f'{user[0]}\t{user[1]}]t{blocked_friends}\t{user[3]}\t{user[4]}\t{user[2]}\n')
-                print(f'{user[0]}\t{user[1]}]t{blocked_friends}\t{user[3]}\t{user[4]}\t{user[2]}')
-                time.sleep(max(0.0, 60.0 + t_begin - time.time()))
+                fout.write(f'{user[0]}\t{user[1]}\t{blocked_friends}\t{user[3]}\t{user[4]}\t{user[2]}\n')
+                if blocked_friends > -1:
+                    print(f'{user[0]:<23}\t{user[1]:15}\t{blocked_friends}\t\t\t\t\t{user[3]}\t\t\t{user[4]}\t\t\t{user[2]}')
+                fout.flush()                                        # Save progress after each user
+                time.sleep(max(0.0, 60.0 + t_begin - time.time()))  # Sleep to avoid rate-limit
                 break
             except:
+                if resp.status_code == 429:
+                    print('Rate-limited! Waiting a little while',end='',flush=True)
+                    for x in range(15):
+                        time.sleep(30)
+                        print('.',end='',flush=True)
+                    print()
+                    continue
+                if user[5]: # If the account is protected
+                    print(f'User {user[0]} (@{user[1]}) has a protected account which you do not follow. Unable to see their friends.')
+                    break
                 if attempt == 2:
                     print(f'There was a problem getting the first 5000 followers for {user[0]} (@{user[1]}). Skipping them.')
     print('Done tabulating blocked friends for each of your followers.')
